a155d8ae1c0912f6a737b1a989764226b1a436b9
朗姆酒 <48828153+afterCherry@users.noreply.github.com>
2023-07-01 23:00:55 +0800

    Create multiply two complex numbers.sv

---
+++ Nowcoder/multiply two complex numbers.sv
@@ -0,0 +1,10 @@
+(a+bi)*(c+di)=(ac-bd)+(ad+bc)i
+4 multiplys, 3 adds
+
+optimization:
+A = (a+b)*c
+B = (c+d)*b
+C = (b-a)*d
+(a+bi)*(c+di) = (A-B)+(B-C)i
+3 multiplys, 6 adds
+Add is much faster than multiply.
